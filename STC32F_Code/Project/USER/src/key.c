c60119bac5c4da994052b40b4f62f613e1d3a18c
Alaye-Dong <AlayeVast@outlook.com>
2024-04-21 16:56:44 +0800

    style: key.c

--- STC32F_Code/Project/USER/src/key.c
+++ STC32F_Code/Project/USER/src/key.c
@@ -1,35 +1,24 @@
 #include "key.h"
 
-//定义按键引脚
-#define KEY1_PIN    P50
-#define KEY2_PIN    P51
-#define KEY3_PIN    P16
-#define KEY4_PIN    P17
+// 定义按键引脚
+#define KEY1_PIN P50
+#define KEY2_PIN P51
+#define KEY3_PIN P16
+#define KEY4_PIN P17
 
-// //定义拨码开关引??
-// #define SW1_PIN     P76
-// #define SW2_PIN     P75
-
-// //拨码开关状态变?? ON??0
-// uint8 sw1_status;
-// uint8 sw2_status;
-
-uint8 keystroke_label = 0;   //按压状态
-uint8 key_last_status[5]={0};
-uint8 key_status[5]={0};
-uint8 key_flag[5]={0};
+uint8 keystroke_label = 0; // 按压状态
+uint8 key_last_status[5] = {0};
+uint8 key_status[5] = {0};
+uint8 key_flag[5] = {0};
 
 void Keystroke_Scan(void)
 {
     uint8 i = 0;
     keystroke_label = 0;
-    
-    // //获取拨码开关状态
-    // sw1_status = SW1_PIN;
-    // sw2_status = SW2_PIN;
-    
+
     // 保存按键状态
-    for ( i = 0; i < 4; i++) {
+    for (i = 0; i < 4; i++)
+    {
         key_last_status[i] = key_status[i];
     }
     key_status[0] = KEY1_PIN;
@@ -37,11 +26,14 @@ void Keystroke_Scan(void)
     key_status[2] = KEY3_PIN;
     key_status[3] = KEY4_PIN;
 
-    for ( i = 0; i < 4; i++) {
-        if (key_status[i] && !key_last_status[i]) {
+    for (i = 0; i < 4; i++)
+    {
+        if (key_status[i] && !key_last_status[i])
+        {
             key_flag[i] = 1;
         }
-        if (key_flag[i]) {
+        if (key_flag[i])
+        {
             key_flag[i] = 0; // 使用按键之后，应该清除标志位
             keystroke_label = i + 1;
             BEEP = 1;
