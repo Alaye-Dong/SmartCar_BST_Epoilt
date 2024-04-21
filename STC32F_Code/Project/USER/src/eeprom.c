feb8413a77f7cad46e672f00da3b2eefc64b3436
Alaye-Dong <AlayeVast@outlook.com>
2024-04-21 16:10:22 +0800

    refactor eeprom.c

--- STC32F_Code/Project/USER/src/eeprom.c
+++ STC32F_Code/Project/USER/src/eeprom.c
@@ -8,29 +8,24 @@ void eeprom_init()
 
     iap_read_bytes(0x00, date_buff, 100); // 从EEPROM中读取数据
 
-    /****不用eeprom直接赋值****/
-
-    /****用eeprom赋值****/
-
-    // 刷写程序后初次启动,填充eeprom
+    // 刷写程序后初次启动,使用源码初始化的值填充eeprom
     eeprom_init_time = read_int(0);
     if (eeprom_init_time != 1)
     {
-        save_int(1, 0);
+        eeprom_init_time++;
+        save_int(eeprom_init_time, 0);
 
-        save_int(start_flag, 1);
-        save_int(garage_out_direction, 2);
-        save_float(PID_P, 3);
-        save_float(PID_D, 4);
-        save_int(normal_speed, 5);
+        eeprom_flash();
     }
-
-    // eeprom读取
-    start_flag = read_int(1);
-    garage_out_direction = read_int(2);
-    PID_P = read_float(3);
-    PID_D = read_float(4);
-    normal_speed = read_int(5);
+    else
+    {
+        // eeprom读取,用eeprom赋值变量
+        start_flag = read_int(1);
+        garage_out_direction = read_int(2);
+        PID_P = read_float(3);
+        PID_D = read_float(4);
+        normal_speed = read_int(5);
+    }
 }
 
 // 刷写保存数据到eeprom
@@ -48,6 +43,7 @@ void save_int(int32 input, uint8 value_bit)
     uint8 i;
     uint8 begin = value_bit * 4;
     uint8 *p = (uint8 *)&input;
+
     for (i = 0; i < 4; i++)
     {
         date_buff[begin++] = *(p + i);
@@ -61,7 +57,7 @@ int32 read_int(uint8 value_bit)
     uint8 begin = value_bit * 4;
     int32 output;
     uint8 *p = (uint8 *)&output;
-    ;
+
     for (i = 0; i < 4; i++)
     {
         *(p + i) = date_buff[begin++];
@@ -74,19 +70,21 @@ void save_float(float input, uint8 value_bit)
     uint8 i;
     uint8 begin = value_bit * 4;
     uint8 *p = (uint8 *)&input;
+
     for (i = 0; i < 4; i++)
     {
         date_buff[begin++] = *(p + i);
     }
     extern_iap_write_bytes(0x00, date_buff, 100);
 }
+
 float read_float(uint8 value_bit)
 {
     uint8 i;
     uint8 begin = value_bit * 4;
     float output;
     uint8 *p = (uint8 *)&output;
-    ;
+
     for (i = 0; i < 4; i++)
     {
         *(p + i) = date_buff[begin++];
