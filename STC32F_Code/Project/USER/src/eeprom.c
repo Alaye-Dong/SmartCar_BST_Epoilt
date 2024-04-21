221b389a2719bdfd4f3b568c9d7391ae8cc49a76
Alaye-Dong <AlayeVast@outlook.com>
2024-04-21 15:37:54 +0800

    style： eeprom.c

--- STC32F_Code/Project/USER/src/eeprom.c
+++ STC32F_Code/Project/USER/src/eeprom.c
@@ -1,55 +1,54 @@
 #include "headfile.h"
 
-uint8 date_buff[100]; //eeprom数据数组
+uint8 date_buff[100]; // eeprom数据数组
 uint8 eeprom_init_time = 0;
 void eeprom_init()
 {
-    iap_init();									//初始化EEPROM;
+    iap_init(); // 初始化EEPROM;
 
-    iap_read_bytes(0x00, date_buff, 100);		//从EEPROM中读取数据
+    iap_read_bytes(0x00, date_buff, 100); // 从EEPROM中读取数据
 
-/****不用eeprom直接赋值****/
+    /****不用eeprom直接赋值****/
 
-	
-/****用eeprom赋值****/
-    
-    //刷写程序后初次启动,填充eeprom
-    eeprom_init_time = read_int(0);	
-    if(eeprom_init_time != 1)
+    /****用eeprom赋值****/
+
+    // 刷写程序后初次启动,填充eeprom
+    eeprom_init_time = read_int(0);
+    if (eeprom_init_time != 1)
     {
-        save_int(1,0);
-        
-        save_int(start_flag,1);
-        save_int(garage_out_direction,2);
-        save_float(PID_P,3);
-        save_float(PID_D,4);
-        save_int(normal_speed,5);
+        save_int(1, 0);
+
+        save_int(start_flag, 1);
+        save_int(garage_out_direction, 2);
+        save_float(PID_P, 3);
+        save_float(PID_D, 4);
+        save_int(normal_speed, 5);
     }
-    
-    //eeprom读取
+
+    // eeprom读取
     start_flag = read_int(1);
     garage_out_direction = read_int(2);
-	PID_P = read_float(3);
+    PID_P = read_float(3);
     PID_D = read_float(4);
     normal_speed = read_int(5);
 }
 
+// 刷写保存数据到eeprom
 void eeprom_flash()
 {
-    //需要保存的参数
-    save_int(start_flag,1);
-    save_int(garage_out_direction,2);
-    save_float(PID_P,3);
-    save_float(PID_D,4);
-    save_int(normal_speed,5);
+    save_int(start_flag, 1);
+    save_int(garage_out_direction, 2);
+    save_float(PID_P, 3);
+    save_float(PID_D, 4);
+    save_int(normal_speed, 5);
 }
 
 void save_int(int32 input, uint8 value_bit)
 {
     uint8 i;
     uint8 begin = value_bit * 4;
-    uint8* p = (uint8*)&input;
-    for(i = 0; i < 4; i++)
+    uint8 *p = (uint8 *)&input;
+    for (i = 0; i < 4; i++)
     {
         date_buff[begin++] = *(p + i);
     }
@@ -61,8 +60,9 @@ int32 read_int(uint8 value_bit)
     uint8 i;
     uint8 begin = value_bit * 4;
     int32 output;
-    uint8* p = (uint8*)&output;;
-    for(i = 0; i < 4; i++)
+    uint8 *p = (uint8 *)&output;
+    ;
+    for (i = 0; i < 4; i++)
     {
         *(p + i) = date_buff[begin++];
     }
@@ -73,8 +73,8 @@ void save_float(float input, uint8 value_bit)
 {
     uint8 i;
     uint8 begin = value_bit * 4;
-    uint8* p = (uint8*)&input;
-    for(i = 0; i < 4; i++)
+    uint8 *p = (uint8 *)&input;
+    for (i = 0; i < 4; i++)
     {
         date_buff[begin++] = *(p + i);
     }
@@ -85,8 +85,9 @@ float read_float(uint8 value_bit)
     uint8 i;
     uint8 begin = value_bit * 4;
     float output;
-    uint8* p = (uint8*)&output;;
-    for(i = 0; i < 4; i++)
+    uint8 *p = (uint8 *)&output;
+    ;
+    for (i = 0; i < 4; i++)
     {
         *(p + i) = date_buff[begin++];
     }
