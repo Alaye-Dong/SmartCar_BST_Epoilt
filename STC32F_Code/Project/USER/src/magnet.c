40c636413d4a01a0b25a5941f2cd4227acf51283
Alaye-Dong <AlayeVast@outlook.com>
2024-05-06 21:01:28 +0800

    🌈 style(magnet.c):

--- STC32F_Code/Project/USER/src/magnet.c
+++ STC32F_Code/Project/USER/src/magnet.c
@@ -1,17 +1,11 @@
 #include "magnet.h"
 
-/**********全局变量定义********/
-
-float g_fDirectionError[5];     // 方向偏差（g_fDirectionError[0]为一对水平电感的差比和偏差）
-float g_fDirectionError_dot[3]; // 方向偏差微分（g_fDirectionError_dot[0]为一对水平电感的差比和偏差微分）
-
-int16 ADC_single_divided_value[6] = {0}; // 获取的电感值
-int16 ADC_filtered_value[5] = {0};
-
-// 定义数组大小常量，提高可读性和可维护性
 #define INDUCTOR 6 // 电感的个数
 #define SAMPLES 5  // 单次采集次数
+
 int16 ADC_value[INDUCTOR][SAMPLES];
+int16 ADC_single_divided_value[INDUCTOR] = {0}; // 获取的电感值
+int16 ADC_filtered_value[INDUCTOR] = {0};
 
 void Magnet_ADC_Init(void)
 {
@@ -26,15 +20,15 @@ void Magnet_ADC_Init(void)
 void Magnet_ADC_Read(void)
 {
     uint8 i;
-    // [左横 3] [左斜 4] [左竖 0] || [右竖 5] [右斜 1] [右横 2]
+    // 牛爷爷的车队 [左横 3] [左斜 4] [左竖 0] || [右竖 5] [右斜 1] [右横 2]
     for (i = 0; i < SAMPLES; i++)
     {
-        ADC_value[0][i] = adc_once(ADC_P11, ADC_12BIT); // zui you
-        ADC_value[1][i] = adc_once(ADC_P05, ADC_12BIT); // zhong jian
-        ADC_value[2][i] = adc_once(ADC_P00, ADC_12BIT); // 横
-        ADC_value[3][i] = adc_once(ADC_P13, ADC_12BIT); // 横
-        ADC_value[4][i] = adc_once(ADC_P06, ADC_12BIT); // zui zuo
-        ADC_value[5][i] = adc_once(ADC_P01, ADC_12BIT); // 05     /
+        ADC_value[0][i] = adc_once(ADC_P11, ADC_12BIT);
+        ADC_value[1][i] = adc_once(ADC_P05, ADC_12BIT);
+        ADC_value[2][i] = adc_once(ADC_P00, ADC_12BIT);
+        ADC_value[3][i] = adc_once(ADC_P13, ADC_12BIT);
+        ADC_value[4][i] = adc_once(ADC_P06, ADC_12BIT);
+        ADC_value[5][i] = adc_once(ADC_P01, ADC_12BIT);
     }
 }
 
@@ -46,33 +40,36 @@ void Magnet_ADC_Filter(void)
 
     Magnet_ADC_Read();
 
-    /*=========================冒泡排序升序==========================*/ // 舍弃最大值和最小值
-    // 调用排序函数，对每一行进行排序
+    /*=========================冒泡排序升序==========================*/
     Bubble_Sort_ADC();
 
     /*===========================中值滤波=================================*/
-    for (i = 0; i < INDUCTOR; i++) 
+    for (i = 0; i < INDUCTOR; i++)
     {
-        for(j = 1; j < SAMPLES - 1; j++) // 求去除最大和最小项的和
+        for (j = 1; j < SAMPLES - 1; j++) // 求去除最大和最小项的和
         {
             ADC_sum[i] += ADC_value[i][j];
         }
-        
+
         ADC_median_value[i] = ADC_sum[i] / (SAMPLES - 2);
     }
 
-    for (i = 0; i < INDUCTOR; i++) // 将数值中个位数除掉
+    for (i = 0; i < INDUCTOR; i++)
     {
-        ADC_single_divided_value[i] = (int16)(ADC_median_value[i] / 10 * 10);
+        ADC_single_divided_value[i] = (int16)(ADC_median_value[i] / 10 * 10); // 将数值中个位数除掉
 
         // 采集梯度平滑，每次采集最大变化40
         ADC_old_filtered_value[i] = ADC_filtered_value[i];
         ADC_new_filtered_value[i] = ADC_single_divided_value[i];
 
         if (ADC_new_filtered_value[i] >= ADC_old_filtered_value[i])
+        {
             ADC_filtered_value[i] = ((ADC_new_filtered_value[i] - ADC_old_filtered_value[i]) > 50 ? (ADC_old_filtered_value[i] + 50) : ADC_new_filtered_value[i]);
+        }
         else
+        {
             ADC_filtered_value[i] = ((ADC_new_filtered_value[i] - ADC_old_filtered_value[i]) < -60 ? (ADC_old_filtered_value[i] - 60) : ADC_new_filtered_value[i]);
+        }
     }
 }
 
