dccdb916cdcf0ff5ae0cf90922d54d05a2a3d11c
Alaye-Dong <AlayeVast@outlook.com>
2024-05-16 12:38:52 +0800

    🎈 perf: 修正归一化的最大值

--- STC32F_Code/Project/USER/src/magnet.c
+++ STC32F_Code/Project/USER/src/magnet.c
@@ -82,11 +82,11 @@ void Bubble_Sort_ADC(void)
 void Inductor_Normal(void)
 {
     // (x - min) / (max - min) * 100
-    inductor_normal_value[LEFT_V] = (float)(ADC_filtered_value[LEFT_V] - 10.0) / (1730.0 - 10.0) * 100.0;
-    inductor_normal_value[RIGHT_V] = (float)(ADC_filtered_value[RIGHT_V] - 10.0) / (1640.0 - 10.0) * 100.0;
+    inductor_normal_value[LEFT_V] = (float)(ADC_filtered_value[LEFT_V] - 10.0) / (3700.0 - 10.0) * 100.0;
+    inductor_normal_value[RIGHT_V] = (float)(ADC_filtered_value[RIGHT_V] - 10.0) / (3700.0 - 10.0) * 100.0;
 
-    inductor_normal_value[LEFT_H] = (float)(ADC_filtered_value[LEFT_H] - 10.0) / (2400.0 - 10.0) * 100.0;
-    inductor_normal_value[RIGHT_H] = (float)(ADC_filtered_value[RIGHT_H] - 10.0) / (2150.0 - 10.0) * 100.0;
+    inductor_normal_value[LEFT_H] = (float)(ADC_filtered_value[LEFT_H] - 10.0) / (3000.0 - 10.0) * 100.0;
+    inductor_normal_value[RIGHT_H] = (float)(ADC_filtered_value[RIGHT_H] - 10.0) / (3000.0 - 10.0) * 100.0;
 
     inductor_normal_value[LEFT_S] = (float)(ADC_filtered_value[LEFT_S] - 10.0) / (2280.0 - 10.0) * 100.0;
     inductor_normal_value[RIGHT_S] = (float)(ADC_filtered_value[RIGHT_S] - 10.0) / (2280.0 - 10.0) * 100.0;
