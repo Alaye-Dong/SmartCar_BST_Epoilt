b5b4d2e8cab10515b2283af45aa6dc5c85622738
Alaye-Dong <AlayeVast@outlook.com>
2024-05-24 01:50:47 +0800

    🎈 perf: 可用的方向PID

--- STC32F_Code/Project/USER/src/pid.c
+++ STC32F_Code/Project/USER/src/pid.c
@@ -22,7 +22,7 @@ void PID_Parameter_Init(PIDTypeDef *sptr, float KP, float KI, float KD, float KP
 
 void PIDType_Init(void)
 {
-    PID_Parameter_Init(&direction, 0.6, 0.6, 0.0, 0.0, 0.0);
+    PID_Parameter_Init(&direction, 0.45, 0.0, 0.95, 0.003, 0.002);
     // PID_Parameter_Init(&motor_left, 1.8, 2.0, 0.0, 0.0, 0.0); //+-5
     // PID_Parameter_Init(&motor_right, 2.0, 2.5, 0.0, 0.0, 0.0);
 
@@ -62,13 +62,9 @@ void PID_Process(void)
 //  转向内环 PD 二次项 PD
 void Direction_PID(void)
 {
-    direction_output = position * direction.KP + (position - position_last) * direction.KD;
-    // + abs(position) * position * direction.KP_2 + gyro_z_filtered * direction.KD_2; //加入二次项，转向更迅速，直道灵敏度降低。融合陀螺仪，转向增加阻尼，更平稳。
-
-    if (FUNC_ABS(position) <=10)
-    {
-        direction_output = 0;
-    }
+    direction_output = position * direction.KP + (position - position_last) * direction.KD
+     + abs(position) * position * direction.KP_2 + gyro_z_filtered * direction.KD_2;
+    // 加入二次项，转向更迅速，直道灵敏度降低。融合陀螺仪，转向增加阻尼，更平稳。
 
     position_last = position;
 
@@ -92,34 +88,32 @@ void Right_Speed_PID(void)
     motor_right_last_error = motor_right_error;
 }
 
-/*************************************并级PID
+//*************************************并级PID
 
-// 转向 PD 二次项 PD
-void Direction_PID(void)
-{
-    direction_output = position * direction.KP + (position - position_last) * direction.KD;
-    // + abs(position) * position * direction.KP_2 + gyro_z_filtered * direction.KD_2; //加入二次项，转向更迅速，直道灵敏度降低。融合陀螺仪，转向增加阻尼，更平稳。
-    position_last = position;
-}
+// // 转向 PD 二次项 PD
+// void Direction_PID(void)
+// {
+//     direction_output = position * direction.KP + (position - position_last) * direction.KD;
+//     // + abs(position) * position * direction.KP_2 + gyro_z_filtered * direction.KD_2; //加入二次项，转向更迅速，直道灵敏度降低。融合陀螺仪，转向增加阻尼，更平稳。
+//     position_last = position;
+// }
 
-// 左轮内环 PI
-void Left_Speed_PID(void)
-{
-    motor_left_error = (int16)(target_speed - encoder_left.encoder_now);
-    motor_left_pwm += (motor_left_error - motor_left_last_error) * motor_left.KP + motor_left_error * motor_left.KI;
-    motor_left_last_error = motor_left_error;
+// // 左轮内环 PI
+// void Left_Speed_PID(void)
+// {
+//     motor_left_error = (int16)(target_speed - encoder_left.encoder_now);
+//     motor_left_pwm += (motor_left_error - motor_left_last_error) * motor_left.KP + motor_left_error * motor_left.KI;
+//     motor_left_last_error = motor_left_error;
 
-    motor_left_pwm -= direction_output; // 融合方向控制
-}
+//     motor_left_pwm -= direction_output; // 融合方向控制
+// }
 
-// 右轮内环 PI
-void Right_Speed_PID(void)
-{
-    motor_right_error = (int16)(target_speed - encoder_right.encoder_now);
-    motor_right_pwm += (motor_right_error - motor_right_last_error) * motor_right.KP + motor_right_error * motor_right.KI;
-    motor_right_last_error = motor_right_error;
+// // 右轮内环 PI
+// void Right_Speed_PID(void)
+// {
+//     motor_right_error = (int16)(target_speed - encoder_right.encoder_now);
+//     motor_right_pwm += (motor_right_error - motor_right_last_error) * motor_right.KP + motor_right_error * motor_right.KI;
+//     motor_right_last_error = motor_right_error;
 
-    motor_right_pwm += direction_output; // 融合方向控制
-}
-
-*/
+//     motor_right_pwm += direction_output; // 融合方向控制
+// }
