972d63da87324920ff5c944c33cfce2d33fee4af
Alaye-Dong <AlayeVast@outlook.com>
2024-05-26 16:10:27 +0800

    🐞 fix: 直角转弯方向正负与判断条件

--- STC32F_Code/Project/USER/src/right_angle.c
+++ STC32F_Code/Project/USER/src/right_angle.c
@@ -5,13 +5,13 @@ RIGHT_ANGLE_STATE_enmu right_angle_flag = RIGHT_ANGLE_NONE;
 // 直角元素识别
 void Right_Angle_Recognition(void)
 {
-    if (inductor_left_H <= 10 && inductor_right_H <= 10)
+    if (inductor_left_H <= 15 || inductor_right_H <= 15)
     {
-        if (inductor_left_V >= 80)
+        if (inductor_left_V >= 90 && inductor_right_V <= 20)
         {
             right_angle_flag = RIGHT_ANGLE_TURN_LEFT;
         }
-        else if (inductor_right_V >= 80)
+        else if (inductor_right_V >= 90 && inductor_left_V <= 20)
         {
             right_angle_flag = RIGHT_ANGLE_TURN_RIGHT;
         }
@@ -22,22 +22,26 @@ void Right_Angle_Turn_Process(void)
 {
     if (right_angle_flag == RIGHT_ANGLE_TURN_LEFT)
     {
-        position = -50;
+        BEEP_ON();
+        position = 80;
         IMU_Yaw_Angle_Get_Control(ON);
-        if (yaw_angle >= 90)
+        if (yaw_angle >= 75)
         {
             right_angle_flag = RIGHT_ANGLE_NONE;
             IMU_Yaw_Angle_Get_Control(RESET);
+            BEEP_OFF();
         }
     }
     else if (right_angle_flag == RIGHT_ANGLE_TURN_RIGHT)
     {
-        position = 50;
+        BEEP_ON();
+        position = -80;
         IMU_Yaw_Angle_Get_Control(ON);
-        if (yaw_angle <= -90)
+        if (yaw_angle <= -75)
         {
             right_angle_flag = RIGHT_ANGLE_NONE;
             IMU_Yaw_Angle_Get_Control(RESET);
+            BEEP_OFF();
         }
     }
 }
